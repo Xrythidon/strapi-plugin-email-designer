3e44932b65de5a6cb99ce011bff61115f8c7cebb
Alex Danisz <alexdanisz@live.com>
2023-06-03 18:24:35 -0700

    Update email service to patch template injection vulnerability.

--- server/services/email.js
+++ server/services/email.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const _ = require('lodash');
+const keysDeep = require('@strapi/utils').keysDeep;
+const createStrictInterpolationRegExp = require('@strapi/utils').template.createStrictInterpolationRegExp;
+
 // From: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
 const isValidEmail =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -9,14 +12,13 @@ const decode = require('decode-html');
 const { htmlToText } = require('html-to-text');
 const { isEmpty } = require('lodash');
 
-const templateSettings = {
-  evaluate: /\{\{(.+?)\}\}/g,
-  interpolate: /\{\{=(.+?)\}\}/g,
-  escape: /\{\{-(.+?)\}\}/g,
-};
-
 module.exports = ({ strapi }) => {
-  const templater = (tmpl) => _.template(tmpl, templateSettings);
+  const templater = (tmpl, data) => {
+    const allowedInterpolationVariables = keysDeep(data);
+    const interpolate = createStrictInterpolationRegExp(allowedInterpolationVariables, 'g');
+    return _.template(tmpl, { interpolate, evaluate: false, escape: false });
+  };
+
 
   const isMantainLegacyTemplateActive = () =>
     _.get(strapi.plugins, 'email-designer.config.mantainLegacyTemplate', true);
@@ -88,7 +90,7 @@ module.exports = ({ strapi }) => {
     const templatedAttributes = attributes.reduce(
       (compiled, attribute) =>
         emailTemplate[attribute]
-          ? Object.assign(compiled, { [attribute]: templater(emailTemplate[attribute])(data) })
+          ? Object.assign(compiled, { [attribute]: templater(emailTemplate[attribute], data) })
           : compiled,
       {}
     );
